6057974a53a0725fb94757faee38f5a1cd66b877
Gilles PERRY <gilles.perry@se.com>
2022-07-08 15:57:58 +0200

    create test-matches.R

--- tests/testthat/test-matches.R
+++ tests/testthat/test-matches.R
@@ -1,3 +1,55 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
+  expect_equal(3 * 3, 9)
 })
+
+test_that("interactive works", {
+  result0 <- as.integer(42)
+  expect_identical(result0, 42L)
+  result1 <- "a"
+  expect_identical(result1, "a")
+  # result2 <- 42.000000001
+  # expect_identical(result2, 42)
+  result3 <- 42.00000000000000000001
+  expect_identical(result3, 42)
+  result4 <- 42.1
+  expect_identical(result4, 42, tolerance = 0.15)
+
+  # vector of named values
+  x <- c(a = 1, 2, c = 3)
+
+  # test whether x has names: no error
+  expect_named(x)
+  # test if the names are "a", "b", "c": error
+  # expect_named(x, c("a", "b", "c"))
+
+  expect_error(sin[1], regexp = "subsettable$")
+})
+
+test_that("uss_make_matches works", {
+
+  # is a tibble
+  italy <- uss_make_matches(engsoccerdata::italy, "Italy")
+
+  expect_true(tibble::is_tibble(italy))
+
+  # has right names
+  expect_named(
+    italy,
+    c("country", "tier", "season", "date", "home", "visitor", "goals_home",
+      "goals_visitor")
+  )
+
+  # has right names
+  expect_identical(
+    names(italy),
+    c("country", "tier", "season", "date", "home", "visitor", "goals_home",
+      "goals_visitor")
+  )
+
+  # is country correct
+  expect_identical(italy$country |> unique(), "Italy")
+})
+
+
+
